2292feb335276632069061cc2c763fb3612da15b
darek <darek@darek.com>
2026-04-06 20:44:51 +0200

    Initial commit

---
+++ app/src/main/java/com/darek/crosscountry/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.darek.crosscountry
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.darek.crosscountry.ui.theme.CrossCountryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CrossCountryTheme {
+                CrossCountryApp()
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun CrossCountryApp() {
+    val navController = rememberNavController()
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                AppDestinations.entries.forEach {
+                    NavigationBarItem(
+                        icon = {
+                            Icon(
+                                it.icon,
+                                contentDescription = it.label
+                            )
+                        },
+                        label = { Text(it.label) },
+                        selected = it == currentDestination,
+                        onClick = {
+                            currentDestination = it
+                            navController.navigate(it.name)
+                        }
+                    )
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = AppDestinations.HOME.name,
+            modifier = Modifier.padding(innerPadding)
+        )
+        {
+            composable(AppDestinations.HOME.name) {
+                Screen("Home")
+            }
+            composable(AppDestinations.FAVORITES.name) {
+                Screen("Favorites")
+            }
+            composable(AppDestinations.PROFILE.name) {
+                Screen("Profile")
+            }
+        }
+    }
+}
+
+enum class AppDestinations(
+    val label: String,
+    val icon: ImageVector,
+) {
+    HOME("Home", Icons.Default.Home),
+    FAVORITES("Favorites", Icons.Default.Favorite),
+    PROFILE("Profile", Icons.Default.AccountBox),
+}
+
+@Composable
+fun Screen(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello at screen $name!",
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ScreenPreview() {
+    CrossCountryTheme {
+        Screen("Android")
+    }
+}
