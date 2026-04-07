75fa5fb629fd3ecb0d70f689b36edc958654a17f
darek <darek@darek.com>
2026-04-07 22:47:55 +0200

    First UiCountriesState, display names and flags

--- app/src/main/java/com/darek/crosscountry/ui/ContriesViewModel.kt
+++ app/src/main/java/com/darek/crosscountry/ui/ContriesViewModel.kt
@@ -3,21 +3,51 @@ package com.darek.crosscountry.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.darek.crosscountry.data.CountriesRepository
+import com.darek.crosscountry.data.Country
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+sealed class CountriesIntent {
+    object ScreenReady : CountriesIntent()
+    object FetchCountries : CountriesIntent()
+}
+
+
+sealed class UiCountriesState {
+    object Loading : UiCountriesState()
+    data class Success(val countries: List<Country>) : UiCountriesState()
+    object Error : UiCountriesState()
+}
+
 
 @HiltViewModel
 class CountriesViewModel @Inject constructor(
     private val countriesRepository: CountriesRepository
 ) : ViewModel() {
-    init {
+
+    val _uiState = MutableStateFlow<UiCountriesState>(UiCountriesState.Loading)
+    val uiState: StateFlow<UiCountriesState> = _uiState.asStateFlow()
+
+    fun sendIntent(intent: CountriesIntent) {
+        when (intent) {
+            is CountriesIntent.ScreenReady -> fetchCountries()
+            is CountriesIntent.FetchCountries -> fetchCountries()
+        }
+    }
+
+    private fun fetchCountries() {
+        _uiState.value = UiCountriesState.Loading
         viewModelScope.launch {
-            val countries = countriesRepository.getCountries()
-            // Handle the list of countries (e.g., update UI state)
-            countries.forEach {
-                println(it)
-            }
+            runCatching { countriesRepository.getCountries() }
+                .onSuccess { _uiState.value = UiCountriesState.Success(it) }
+                .onFailure { _uiState.value = UiCountriesState.Error }
         }
     }
 }
--- app/src/main/java/com/darek/crosscountry/ui/countries/CountriesScreen.kt
+++ app/src/main/java/com/darek/crosscountry/ui/countries/CountriesScreen.kt
@@ -1,10 +1,77 @@
 package com.darek.crosscountry.ui.countries
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.darek.crosscountry.data.Country
+import com.darek.crosscountry.ui.CountriesIntent
 import com.darek.crosscountry.ui.CountriesViewModel
+import com.darek.crosscountry.ui.UiCountriesState
 
 @Composable
 fun CountriesScreen(viewModel: CountriesViewModel = hiltViewModel()) {
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
 
+    LaunchedEffect(Unit) {
+        viewModel.sendIntent(CountriesIntent.ScreenReady)
+    }
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        when (uiState.value) {
+            is UiCountriesState.Loading -> LoadingIndicator()
+            is UiCountriesState.Success -> {
+                val countries = (uiState.value as UiCountriesState.Success).countries
+                CountryList(countries)
+            }
+
+            is UiCountriesState.Error -> Text(text = "Error")
+        }
+    }
+}
+
+@Composable
+fun LoadingIndicator() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun CountryList(countries: List<Country>) {
+    LazyColumn {
+        items(countries) { country ->
+            CountryItem(country)
+        }
+    }
+}
+
+@Composable
+fun CountryItem(country: Country) {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(text = country.name.common)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = country.flag)
+    }
 }
